540232a1a7366f71c31624730e5d5278408f0ff8
diegod887 <128403752+diegod887@users.noreply.github.com>
2023-05-22 09:19:49 -0700

    update

---
+++ Game jam 2d programming/Assets/scripts/patrolbehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class patrolbehavior : MonoBehaviour
+{
+    public float speed;
+    public float rayDist;
+    private bool movingRight;
+    public Transform groundDetect
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
